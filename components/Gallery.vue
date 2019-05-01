4b1a0389dd86536d24958f15f7b21bfd09f3a143
Joody Konno <joodykonno@hotmail.com>
2019-05-01 10:15:28 -0300

    Added scss styling
    - Added styles using scss
    - Added the variables.scss to set global variables that will be used on other
    scss files
    - Added reset.scss to give the elements an clean state
    - Added mixin.scss to define functions
    - Added basic.scss to define general classes
    - Added grid.scss to define a basic grid structure

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,16 +1,18 @@
 <template>
   <section>
-    <div v-for="photo in photos" :key="photo.id">
-      <div>
-        <img :src="photo.author.profileImageSmall" />
-        <span>From:</span>
-        <span>{{ photo.author.firstName }} {{ photo.author.lastName }}</span>
-      </div>
-      <nuxt-link :to="`/photo/${photo.id}`">
-        <img :src="photo.thumbImageSrc" :alt="photo.altDescription">
-      </nuxt-link>
-      <div>
-        <span>{{ photo.description }}</span>
+    <div class="columns">
+      <div class="column" v-for="photo in photos" :key="photo.id">
+        <div>
+          <img :src="photo.author.profileImageSmall" />
+          <span>From:</span>
+          <span>{{ photo.author.firstName }} {{ photo.author.lastName }}</span>
+        </div>
+        <nuxt-link :to="`/photo/${photo.id}`">
+          <img :src="photo.thumbImageSrc" :alt="photo.altDescription">
+        </nuxt-link>
+        <div>
+          <span>{{ photo.description }}</span>
+        </div>
       </div>
     </div>
   </section>
